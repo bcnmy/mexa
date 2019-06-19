a79e0537cece3cee92d3865651956313f6a143da
Sachin Tomar <sachin.arox@gmail.com>
2019-06-19 19:54:47 +0530

    Updated identity proxy manager contract

--- contracts/IdentityProxyManager.sol
+++ contracts/IdentityProxyManager.sol
@@ -6,10 +6,14 @@ contract IdentityProxyManager is Ownable {
 
 	address[] public relayers;
 	mapping(address => bool) public relayerStatus;
-	mapping(address => mapping(address => bool)) proxyOwners;
+	mapping(address => mapping(address => bool)) public proxyOwners;
+	mapping(address => address) public proxyOwnerMap;
 
 	// EVENTS
 	event ProxyCreated(address indexed identityProxy, address indexed owner, address creator);
+	event Forwarded (address indexed destination, uint amount, bytes data);
+	event RelayerAdded(address relayer, address owner);
+	event ProxyOwnerAdded(address indexed proxy, address currentOwner, address newOwner);
 
 	// MODIFIERS
 	modifier onlyRelayer() {
@@ -17,8 +21,8 @@ contract IdentityProxyManager is Ownable {
 		_;
 	}
 
-	modifier onlyProxyOwner(address proxy, address owner) {
-		require(proxyOwners[proxy][owner], "You are not the owner of proxy contract");
+	modifier onlyProxyOwner(address owner, address proxy) {
+		require(proxyOwners[owner][proxy], "You are not the owner of proxy contract");
 		_;
 	}
 
@@ -37,18 +41,28 @@ contract IdentityProxyManager is Ownable {
 
 	function createIdentityProxy(address owner) public onlyRelayer {
 		IdentityProxy identityProxy = new IdentityProxy();
-		proxyOwners[address(identityProxy)][owner] = true;
+		proxyOwners[owner][address(identityProxy)] = true;
+		proxyOwnerMap[owner] = address(identityProxy);
 		emit ProxyCreated(address(identityProxy), owner, msg.sender);
 	}
 
+	function getProxyAddress(address owner) public view returns(address) {
+		address proxyAddress = proxyOwnerMap[owner];
+		if(proxyOwners[owner][proxyAddress]) {
+			return proxyAddress;
+		}
+		return address(0);
+	}
+
 	function forward(address payable proxy, address owner, address destination, uint amount, bytes memory data) 
-		public onlyProxyOwner(proxy, owner) onlyRelayer 
+		public onlyProxyOwner(owner, proxy) onlyRelayer 
 	{
 		IdentityProxy identityProxy = IdentityProxy(proxy);
 		identityProxy.forward(destination, amount, data);
+		emit Forwarded(destination, amount, data);
 	}
 
-	function withdraw(address payable proxy, address owner, address payable receiver, uint256 amount) public onlyProxyOwner(proxy, owner) onlyRelayer {
+	function withdraw(address payable proxy, address owner, address payable receiver, uint256 amount) public onlyProxyOwner(owner, proxy) onlyRelayer {
 		IdentityProxy identityProxy = IdentityProxy(proxy);
 		identityProxy.withdraw(receiver, amount);	
 	}
@@ -57,6 +71,13 @@ contract IdentityProxyManager is Ownable {
 		require(!relayerStatus[relayer]);
 		relayers.push(relayer);
 		relayerStatus[relayer] = true;
+		emit RelayerAdded(relayer, msg.sender);
+	}
+
+	function addProxyOwner(address payable proxy, address currentOwner, address newOwner) public onlyProxyOwner(currentOwner, proxy) onlyRelayer {
+		proxyOwners[newOwner][proxy] = true;
+		proxyOwnerMap[newOwner] = proxy;
+		emit ProxyOwnerAdded(proxy, currentOwner, newOwner);
 	}
 
 }
