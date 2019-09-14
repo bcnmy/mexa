4b5306744b935280c6673473c454707dc6be088d
Sachin Tomar <sachin.arox@gmail.com>
2019-09-14 21:23:37 +0530

    Added zero address check while adding relayers

--- contracts/IdentityProxyManager.sol
+++ contracts/IdentityProxyManager.sol
@@ -87,6 +87,7 @@
 		//Register new Relayer
 		function addRelayers(address[] memory relayerArray) public onlyOwner {
 			for(uint i = 0; i<relayerArray.length; i++) {
+				require(relayerArray[i] != address(0), 'Relayer address cannot be zero');
 				relayers.push(relayerArray[i]);
 				relayerStatus[relayerArray[i]] = true;
 				emit RelayerAdded(relayerArray[i], msg.sender);
