de4059ce38d19559858e4a1b386d937f6f699055
Sachin Tomar <sachin.arox@gmail.com>
2020-03-27 18:21:36 +0530

    Transfer ownership after deployment

--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -5,6 +5,9 @@ var ImplementationLogic = artifacts.require("ImplementationLogic");
 var SafeMath = artifacts.require("SafeMath");
 
 module.exports = function (deployer) {
+	// Initialize owner address if you want to transfer ownership of contract to some other address
+	let ownerAddress = "0x7f3B192Ab3220940D66236792F3EBDB0e4E74138";
+
 	deployer.deploy(SafeMath, { overwrite: false });
 	deployer.link(SafeMath, RelayHub);
 
@@ -14,6 +17,13 @@ module.exports = function (deployer) {
 			return deployer.deploy(ProxyManager, ImplementationLogic.address).then(function (proxyManager) {
 				return deployer.deploy(RelayHub, RelayerManager.address, ProxyManager.address).then(async function (relayHub) {
 					await proxyManager.upgradeRelayHub(RelayHub.address);
+					if(ownerAddress && ownerAddress!="") {
+						console.log(`Transfering ownerhip to address ${ownerAddress}`)
+						await proxyManager.transferOwnership(ownerAddress);
+						await relayHub.transferOwnership(ownerAddress);
+						await relayerManager.transferOwnership(ownerAddress);
+						await implementationLogic.transferOwnership(ownerAddress);
+					}
 				});
 			});
 		});
