0b43f8c40bab4aebc15a169dda425dcd8be16dc7
realkinando <kiran@blox.is>
2020-08-19 13:30:27 +0100

    updated exchange-proxy to support personalSign

--- contracts/5/balancer/MexaExchangeProxy.sol
+++ contracts/5/balancer/MexaExchangeProxy.sol
@@ -9,17 +9,13 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-// WE'RE ADDING METATRANSACTION EQUIVALENTS TO ALL METHODS THAT TAKE IN ERC20s EXCLUSIVELY
-
-// GOING CTRL-C CTRL-V WAS A LAZY APPROACH
-// --> WILL PATTERN UP WITH INTERNAL METHODS FOR ALL DUPLICATED FUNCTIONS TO REDUCE BLOAT
-
-pragma solidity 0.5.13;
+pragma solidity 0.5.12;
 pragma experimental ABIEncoderV2;
 
-import "../libs/EIP712BaseB.sol";
+import "../BasicMetaTransaction.sol";
+
 
 contract PoolInterface {
     function swapExactAmountIn(address, uint, address, uint, uint) external returns (uint, uint);
@@ -36,12 +32,7 @@ contract TokenInterface {
     function withdraw(uint) public;
 }
 
-contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
-
-    bytes32 internal constant META_BATCH_SWAP_EXACT_IN_TYPEHASH = keccak256(bytes("MetaBatchSwapExactIn(Swap[] swaps,address tokenIn,address tokenOut,uint totalAmountIn,uint minTotalAmountOut)"));
-    bytes32 internal constant META_BATCH_SWAP_EXACT_OUT_TYPEHASH = keccak256(bytes("MetaBatchSwapExactOut(Swap[] swaps,address tokenIn,address tokenOut,uint maxTotalAmountIn"));
-    bytes32 internal constant META_BATCH_ETH_OUT_SWAP_EXACT_IN_TYPEHASH = keccak256(bytes("MetaBatchEthOutSwapExactIn(Swap[] memory swaps,address tokenIn,uint totalAmountIn,uint minTotalAmountOut)"));
-    bytes32 internal constant META_BATCH_ETH_OUT_SWAP_EXACT_OUT_TYPEHASH = keccak256(bytes("MetaBatchEthOutSwapExactOut(Swap[] memory swaps,address tokenIn,uint maxTotalAmountIn)"));
+contract ExchangeProxy is BasicMetaTransaction{
 
     struct Swap {
         address pool;
@@ -57,7 +48,7 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
     ) anonymous;
 
     modifier _logs_() {
-        emit LOG_CALL(msg.sig, msg.sender, msg.data);
+        emit LOG_CALL(msg.sig, msgSender(), msg.data);
         _;
     }
 
@@ -95,7 +86,7 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
     {
         TokenInterface TI = TokenInterface(tokenIn);
         TokenInterface TO = TokenInterface(tokenOut);
-        require(TI.transferFrom(msg.sender, address(this), totalAmountIn), "ERR_TRANSFER_FAILED");
+        require(TI.transferFrom(msgSender(), address(this), totalAmountIn), "ERR_TRANSFER_FAILED");
         for (uint i = 0; i < swaps.length; i++) {
             Swap memory swap = swaps[i];
             
@@ -113,68 +104,8 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
             totalAmountOut = add(tokenAmountOut, totalAmountOut);
         }
         require(totalAmountOut >= minTotalAmountOut, "ERR_LIMIT_OUT");
-        require(TO.transfer(msg.sender, TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
-        require(TI.transfer(msg.sender, TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
-        return totalAmountOut;
-    }
-
-    function metaBatchSwapExactIn(
-        address from,
-        Swap[] memory swaps,
-        address tokenIn,
-        address tokenOut,
-        uint totalAmountIn,
-        uint minTotalAmountOut,
-        Signature memory signature
-    )   
-        public
-        _logs_
-        _lock_
-        returns (uint totalAmountOut)
-    {
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encode(
-                        META_BATCH_SWAP_EXACT_IN_TYPEHASH,
-                        swaps,
-                        tokenIn,
-                        tokenOut,
-                        totalAmountIn,
-                        minTotalAmountOut
-                    )
-                )
-            )
-        );
-        require(from != address(0), "invalid-address-0");
-        require(
-            from == ecrecover(digest, signature.v, signature.r, signature.s),
-            "invalid-signatures"
-        );
-        TokenInterface TI = TokenInterface(tokenIn);
-        TokenInterface TO = TokenInterface(tokenOut);
-        require(TI.transferFrom(from, address(this), totalAmountIn), "ERR_TRANSFER_FAILED");
-        for (uint i = 0; i < swaps.length; i++) {
-            Swap memory swap = swaps[i];
-            
-            PoolInterface pool = PoolInterface(swap.pool);
-            if (TI.allowance(address(this), swap.pool) < totalAmountIn) {
-                TI.approve(swap.pool, uint(-1));
-            }
-            (uint tokenAmountOut,) = pool.swapExactAmountIn(
-                                        tokenIn,
-                                        swap.tokenInParam,
-                                        tokenOut,
-                                        swap.tokenOutParam,
-                                        swap.maxPrice
-                                    );
-            totalAmountOut = add(tokenAmountOut, totalAmountOut);
-        }
-        require(totalAmountOut >= minTotalAmountOut, "ERR_LIMIT_OUT");
-        require(TO.transfer(from, TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
-        require(TI.transfer(from, TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
+        require(TO.transfer(msgSender(), TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
+        require(TI.transfer(msgSender(), TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
         return totalAmountOut;
     }
 
@@ -191,7 +122,7 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
     {
         TokenInterface TI = TokenInterface(tokenIn);
         TokenInterface TO = TokenInterface(tokenOut);
-        require(TI.transferFrom(msg.sender, address(this), maxTotalAmountIn), "ERR_TRANSFER_FAILED");
+        require(TI.transferFrom(msgSender(), address(this), maxTotalAmountIn), "ERR_TRANSFER_FAILED");
         for (uint i = 0; i < swaps.length; i++) {
             Swap memory swap = swaps[i];
             PoolInterface pool = PoolInterface(swap.pool);
@@ -208,65 +139,8 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
             totalAmountIn = add(tokenAmountIn, totalAmountIn);
         }
         require(totalAmountIn <= maxTotalAmountIn, "ERR_LIMIT_IN");
-        require(TO.transfer(msg.sender, TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
-        require(TI.transfer(msg.sender, TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
-        return totalAmountIn;
-    }
-
-    function metaBatchSwapExactOut(
-        address from,
-        Swap[] memory swaps,
-        address tokenIn,
-        address tokenOut,
-        uint maxTotalAmountIn,
-        Signature memory signature
-    )
-        public
-        _logs_
-        _lock_
-        returns (uint totalAmountIn)
-    {
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encode(
-                        META_BATCH_SWAP_EXACT_OUT_TYPEHASH,
-                        swaps,
-                        tokenIn,
-                        tokenOut,
-                        maxTotalAmountIn
-                    )
-                )
-            )
-        );
-        require(from != address(0), "invalid-address-0");
-        require(
-            from == ecrecover(digest, signature.v, signature.r, signature.s),
-            "invalid-signatures"
-        );
-        TokenInterface TI = TokenInterface(tokenIn);
-        TokenInterface TO = TokenInterface(tokenOut);
-        require(TI.transferFrom(from, address(this), maxTotalAmountIn), "ERR_TRANSFER_FAILED");
-        for (uint i = 0; i < swaps.length; i++) {
-            Swap memory swap = swaps[i];
-            PoolInterface pool = PoolInterface(swap.pool);
-            if (TI.allowance(address(this), swap.pool) < maxTotalAmountIn) {
-                TI.approve(swap.pool, uint(-1));
-            }
-            (uint tokenAmountIn,) = pool.swapExactAmountOut(
-                                        tokenIn,
-                                        swap.tokenInParam,
-                                        tokenOut,
-                                        swap.tokenOutParam,
-                                        swap.maxPrice
-                                    );
-            totalAmountIn = add(tokenAmountIn, totalAmountIn);
-        }
-        require(totalAmountIn <= maxTotalAmountIn, "ERR_LIMIT_IN");
-        require(TO.transfer(from, TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
-        require(TI.transfer(from, TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
+        require(TO.transfer(msgSender(), TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
+        require(TI.transfer(msgSender(), TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
         return totalAmountIn;
     }
 
@@ -298,13 +172,13 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
             totalAmountOut = add(tokenAmountOut, totalAmountOut);
         }
         require(totalAmountOut >= minTotalAmountOut, "ERR_LIMIT_OUT");
-        require(TO.transfer(msg.sender, TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
+        require(TO.transfer(msgSender(), TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
         uint wethBalance = weth.balanceOf(address(this));
         if (wethBalance > 0) {
             weth.withdraw(wethBalance);
-            (bool xfer,) = msg.sender.call.value(wethBalance)("");
+            (bool xfer,) = msgSender().call.value(wethBalance)("");
             require(xfer, "ERR_ETH_FAILED");
-        }                        
+        }
         return totalAmountOut;
     }
 
@@ -320,7 +194,7 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
         returns (uint totalAmountOut)
     {
         TokenInterface TI = TokenInterface(tokenIn);
-        require(TI.transferFrom(msg.sender, address(this), totalAmountIn), "ERR_TRANSFER_FAILED");
+        require(TI.transferFrom(msgSender(), address(this), totalAmountIn), "ERR_TRANSFER_FAILED");
         for (uint i = 0; i < swaps.length; i++) {
             Swap memory swap = swaps[i];
             PoolInterface pool = PoolInterface(swap.pool);
@@ -340,69 +214,9 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
         require(totalAmountOut >= minTotalAmountOut, "ERR_LIMIT_OUT");
         uint wethBalance = weth.balanceOf(address(this));
         weth.withdraw(wethBalance);
-        (bool xfer,) = msg.sender.call.value(wethBalance)("");
+        (bool xfer,) = msgSender().call.value(wethBalance)("");
         require(xfer, "ERR_ETH_FAILED");
-        require(TI.transfer(msg.sender, TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
-        return totalAmountOut;
-    }
-
-    function metaEthOutSwapExactIn(
-        address from,
-        Swap[] memory swaps,
-        address tokenIn,
-        uint totalAmountIn,
-        uint minTotalAmountOut,
-        Signature memory signature
-    )
-        public
-        _logs_
-        _lock_
-        returns (uint totalAmountOut)
-    {
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encode(
-                        META_BATCH_ETH_OUT_SWAP_EXACT_IN_TYPEHASH,
-                        swaps,
-                        tokenIn,
-                        totalAmountIn,
-                        minTotalAmountOut
-                    )
-                )
-            )
-        );
-        require(from != address(0), "invalid-address-0");
-        require(
-            from == ecrecover(digest, signature.v, signature.r, signature.s),
-            "invalid-signatures"
-        );
-        TokenInterface TI = TokenInterface(tokenIn);
-        require(TI.transferFrom(from, address(this), totalAmountIn), "ERR_TRANSFER_FAILED");
-        for (uint i = 0; i < swaps.length; i++) {
-            Swap memory swap = swaps[i];
-            PoolInterface pool = PoolInterface(swap.pool);
-            if (TI.allowance(address(this), swap.pool) < totalAmountIn) {
-                TI.approve(swap.pool, uint(-1));
-            }
-            (uint tokenAmountOut,) = pool.swapExactAmountIn(
-                                        tokenIn,
-                                        swap.tokenInParam,
-                                        address(weth),
-                                        swap.tokenOutParam,
-                                        swap.maxPrice
-                                    );
-
-            totalAmountOut = add(tokenAmountOut, totalAmountOut);
-        }
-        require(totalAmountOut >= minTotalAmountOut, "ERR_LIMIT_OUT");
-        uint wethBalance = weth.balanceOf(address(this));
-        weth.withdraw(wethBalance);
-        (bool xfer,) = from.call.value(wethBalance)("");
-        require(xfer, "ERR_ETH_FAILED");
-        require(TI.transfer(from, TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
+        require(TI.transfer(msgSender(), TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
         return totalAmountOut;
     }
 
@@ -433,11 +247,11 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
 
             totalAmountIn = add(tokenAmountIn, totalAmountIn);
         }
-        require(TO.transfer(msg.sender, TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
+        require(TO.transfer(msgSender(), TO.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
         uint wethBalance = weth.balanceOf(address(this));
         if (wethBalance > 0) {
             weth.withdraw(wethBalance);
-            (bool xfer,) = msg.sender.call.value(wethBalance)("");
+            (bool xfer,) = msgSender().call.value(wethBalance)("");
             require(xfer, "ERR_ETH_FAILED");
         }
         return totalAmountIn;
@@ -454,7 +268,7 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
         returns (uint totalAmountIn)
     {
         TokenInterface TI = TokenInterface(tokenIn);
-        require(TI.transferFrom(msg.sender, address(this), maxTotalAmountIn), "ERR_TRANSFER_FAILED");
+        require(TI.transferFrom(msgSender(), address(this), maxTotalAmountIn), "ERR_TRANSFER_FAILED");
         for (uint i = 0; i < swaps.length; i++) {
             Swap memory swap = swaps[i];
             PoolInterface pool = PoolInterface(swap.pool);
@@ -472,68 +286,10 @@ contract MexaExchangeProxy is EIP712BaseB("MexaExchangeProxy","1",42){
             totalAmountIn = add(tokenAmountIn, totalAmountIn);
         }
         require(totalAmountIn <= maxTotalAmountIn, "ERR_LIMIT_IN");
-        require(TI.transfer(msg.sender, TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
+        require(TI.transfer(msgSender(), TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
         uint wethBalance = weth.balanceOf(address(this));
         weth.withdraw(wethBalance);
-        (bool xfer,) = msg.sender.call.value(wethBalance)("");
-        require(xfer, "ERR_ETH_FAILED");
-        return totalAmountIn;
-    }
-
-    function metaBatchEthOutSwapExactOut(
-        address from,
-        Swap[] memory swaps,
-        address tokenIn,
-        uint maxTotalAmountIn,
-        Signature memory signature
-    )
-        public
-        _logs_
-        _lock_
-        returns (uint totalAmountIn)
-    {
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encode(
-                        META_BATCH_ETH_OUT_SWAP_EXACT_OUT_TYPEHASH,
-                        swaps,
-                        tokenIn,
-                        maxTotalAmountIn
-                    )
-                )
-            )
-        );
-        require(from != address(0), "invalid-address-0");
-        require(
-            from == ecrecover(digest, signature.v, signature.r, signature.s),
-            "invalid-signatures"
-        );
-        TokenInterface TI = TokenInterface(tokenIn);
-        require(TI.transferFrom(from, address(this), maxTotalAmountIn), "ERR_TRANSFER_FAILED");
-        for (uint i = 0; i < swaps.length; i++) {
-            Swap memory swap = swaps[i];
-            PoolInterface pool = PoolInterface(swap.pool);
-            if (TI.allowance(address(this), swap.pool) < maxTotalAmountIn) {
-                TI.approve(swap.pool, uint(-1));
-            }
-            (uint tokenAmountIn,) = pool.swapExactAmountOut(
-                                        tokenIn,
-                                        swap.tokenInParam,
-                                        address(weth),
-                                        swap.tokenOutParam,
-                                        swap.maxPrice
-                                    );
-
-            totalAmountIn = add(tokenAmountIn, totalAmountIn);
-        }
-        require(totalAmountIn <= maxTotalAmountIn, "ERR_LIMIT_IN");
-        require(TI.transfer(from, TI.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
-        uint wethBalance = weth.balanceOf(address(this));
-        weth.withdraw(wethBalance);
-        (bool xfer,) = from.call.value(wethBalance)("");
+        (bool xfer,) = msgSender().call.value(wethBalance)("");
         require(xfer, "ERR_ETH_FAILED");
         return totalAmountIn;
     }
