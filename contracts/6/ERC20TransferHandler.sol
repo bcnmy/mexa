16eecb4c546b9730e9a6eecda9f874073313aadf
realkinando <kiran@blox.is>
2020-09-11 12:42:18 +0100

    erc20Forwarding bug fixed, directory clean up due next

---
+++ contracts/6/ERC20TransferHandler.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.6.8;
+pragma experimental ABIEncoderV2;
+
+//to do, seperate into forwarderWithPersonalSign.sol and ERC20Forwarder.sol
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./BaseRelayRecipient.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20TransferHandler is BaseRelayRecipient{
+
+    constructor(address _forwarder) public {
+        trustedForwarder = _forwarder;
+    }
+
+    function versionRecipient() external virtual view override returns (string memory){ return "1";}
+
+    function transfer(address token, address to, uint256 amount) external{
+        require(IERC20(token).transferFrom(_msgSender(),to,amount));
+    }
+
+}
---
+++ contracts/6/PersonalSignForwarder.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.6.8;
+pragma experimental ABIEncoderV2;
+
+//to do, seperate into forwarderWithPersonalSign.sol and ERC20Forwarder.sol
+
+import "./Forwarder.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract PersonalSignForwarder is Forwarder {
+
+    function executePersonalSign(ForwardRequest memory req,bytes32 domainSeparator,bytes32 requestTypeHash,bytes calldata suffixData,bytes calldata sig) 
+    external payable 
+    returns(bool success, bytes memory ret){
+        _verifyNonce(req);
+        _verifySigPersonalSign(req, domainSeparator, requestTypeHash, suffixData, sig);
+        _updateNonce(req);
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (success,ret) = req.to.call{gas : req.gas, value : req.value}(abi.encodePacked(req.data, req.from));
+        if ( address(this).balance>0 ) {
+            //can't fail: req.from signed (off-chain) the request, so it must be an EOA...
+            payable(req.from).transfer(address(this).balance);
+        }
+        return (success,ret);
+    }
+
+    function prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
+
+    function _verifySigPersonalSign(
+        ForwardRequest memory req,
+        bytes32 domainSeparator,
+        bytes32 requestTypeHash,
+        bytes memory suffixData,
+        bytes memory sig)
+    internal
+    view
+    {
+
+        require(typeHashes[requestTypeHash], "invalid request typehash");
+        bytes32 digest = prefixed(keccak256(abi.encodePacked(
+                "\x19\x01", domainSeparator,
+                keccak256(_getEncoded(req, requestTypeHash, suffixData))
+            )));
+        require(digest.recover(sig) == req.from, "signature mismatch");
+    }
+
+}
