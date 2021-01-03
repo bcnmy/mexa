c488dbad8b1e89d716c0ccd5cdb849ffeb890623
divya <divya@biconomy.io>
2021-01-03 15:28:43 +0530

    commission calculation in while sending funds

--- contracts/6/insta-swaps/LiquidityPoolManager.sol
+++ contracts/6/insta-swaps/LiquidityPoolManager.sol
@@ -14,21 +14,18 @@ contract LiquidityPoolManager is ReentrancyGuard, Ownable, BaseRelayRecipient, P
 
     address private constant NATIVE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
+
+    uint256 public biconomyCommission;
     mapping ( address => bool ) public tokenMap;    // tokenAddress => active_status
     mapping ( address => uint256) public tokenBalanceMap;
     mapping ( address => mapping ( address => uint256) ) public liquidityProviderMap; // tokenAddress => LPaddress => amount
+    mapping ( address => mapping ( address => uint256) ) public rewardMap;
     mapping ( bytes32 => bool ) public sendFundsMap;
     ExecutorManager executorManager;
 
-    struct ReceiverData {
-		address _tokenAddress;
-        uint256 _amount;
-        address payable _destinations;
-        string _depositHash;
-	}
-
     event AssetSent(address indexed asset, uint256 amount, address target);
     event Received(address from, uint256 amount);
+    event Deposit(address tokenAddress, address receiver, uint256 amount);
 
     // MODIFIERS
     modifier onlyExecutorOrOwner() {
@@ -38,11 +35,24 @@ contract LiquidityPoolManager is ReentrancyGuard, Ownable, BaseRelayRecipient, P
         _;
     }
 
-    constructor(address _executorManagerAddress,  address owner) public Ownable(owner) Pausable(owner) {
+    modifier tokenChecks(){
+        require(tokenAddress != address(0), "Token address cannot be 0");
+        require(tokenMap[tokenAddress], "Token not supported");
+
+        _;
+    }
+
+    constructor(address _executorManagerAddress,  address owner, uint256 _commissionRate) public Ownable(owner) Pausable(owner) {
         require(_executorManagerAddress != address(0), "ExecutorManager Contract Address cannot be 0");
         executorManager = ExecutorManager(_executorManagerAddress); 
         tokenMap[NATIVE] = true;
         trustedForwarder = address(this);
+        biconomyCommission = _commissionRate;
+    }
+
+    function changeCommissionRate(uint256 newCommissionRate) public Ownable(owner) Pausable(owner) {
+        require(newCommissionRate != 0, "Commission Rate cannot be 0");
+        biconomyCommission = newCommissionRate;
     }
 
     function versionRecipient() external override virtual view returns (string memory){
@@ -58,33 +68,29 @@ contract LiquidityPoolManager is ReentrancyGuard, Ownable, BaseRelayRecipient, P
         trustedForwarder = forwarderAddress;
     }
 
-    function addSupportedToken( address tokenAddress ) public onlyOwner {
-        require(tokenAddress != address(0), "Token address cannot be 0");
+    function addSupportedToken( address tokenAddress ) public tokenChecks onlyOwner {
         tokenMap[tokenAddress] = true;
     }
 
-    function removeSupportedToken( address tokenAddress ) public onlyOwner {
-        require(tokenAddress != address(0), "Token address cannot be 0");
+    function removeSupportedToken( address tokenAddress ) public tokenChecks onlyOwner {
         require(tokenAddress != NATIVE, "Native currency can't be removed");
         tokenMap[tokenAddress] = false;
     }
 
-    function addEthLiquidity() public payable onlyExecutorOrOwner whenNotPaused {
+    function addEthLiquidity() public payable whenNotPaused {
         require(msg.value > 0, "amount should be greater then 0");
         liquidityProviderMap[NATIVE][_msgSender()] = liquidityProviderMap[NATIVE][_msgSender()].add(msg.value);
         tokenBalanceMap[NATIVE] = tokenBalanceMap[NATIVE].add(msg.value);
     }
 
-    function removeEthLiquidity(uint256 amount) public onlyExecutorOrOwner whenNotPaused {
+    function removeEthLiquidity(uint256 amount) public whenNotPaused {
         require(liquidityProviderMap[NATIVE][_msgSender()] >= amount, "Not enough balance");
-        require(_msgSender().send(amount), "Native Transfer Failed");
         liquidityProviderMap[NATIVE][_msgSender()] = liquidityProviderMap[NATIVE][_msgSender()].sub(amount);
         tokenBalanceMap[NATIVE] = tokenBalanceMap[NATIVE].sub(amount);
+        require(_msgSender().send(amount), "Native Transfer Failed");
     }
 
-    function addTokenLiquidity( address tokenAddress, uint256 amount ) public onlyExecutorOrOwner whenNotPaused {
-        require(tokenAddress != address(0), "Token address cannot be 0");
-        require(tokenMap[tokenAddress], "Token not supported");
+    function addTokenLiquidity( address tokenAddress, uint256 amount ) public tokenChecks whenNotPaused {
         require(amount > 0, "amount should be greater then 0");
 
         IERC20 erc20 = IERC20(tokenAddress);
@@ -95,8 +101,7 @@ contract LiquidityPoolManager is ReentrancyGuard, Ownable, BaseRelayRecipient, P
         tokenBalanceMap[tokenAddress] = tokenBalanceMap[tokenAddress].add(amount);
     }
 
-    function removeTokenLiquidity( address tokenAddress, uint256 amount ) public onlyExecutorOrOwner whenNotPaused {
-        require(tokenAddress != address(0), "Token address cannot be 0");
+    function removeTokenLiquidity( address tokenAddress, uint256 amount ) public tokenChecks whenNotPaused {
         require(amount > 0, "amount should be greater then 0");
         require(liquidityProviderMap[tokenAddress][_msgSender()] >= amount, "Not enough balance");
 
@@ -105,39 +110,45 @@ contract LiquidityPoolManager is ReentrancyGuard, Ownable, BaseRelayRecipient, P
 
         liquidityProviderMap[tokenAddress][_msgSender()] = liquidityProviderMap[tokenAddress][_msgSender()].sub(amount);
         tokenBalanceMap[tokenAddress] = tokenBalanceMap[tokenAddress].sub(amount);
-
     }
 
-    function send( address tokenAddress, uint256 amount, address payable destination, string memory depositHash ) public nonReentrant onlyExecutorOrOwner whenNotPaused {
-        require(destination != address(0), "Bad destination");
+    function deposit( address tokenAddress, address receiver, uint256 amount ) public tokenChecks onlyExecutorOrOwner whenNotPaused {
+        require(receiver != address(0), "Receiver address cannot be 0");
+        require(amount > 0, "amount should be greater then 0");
+
+        IERC20 erc20 = IERC20(tokenAddress);
+
+        require(erc20.transferFrom(_msgSender(),address(this),amount),"Deposit Failed");
+
+        emit Deposit(tokenAddress, receiver, amount);
+    }
+
+    function send( address tokenAddress, uint256 amount, address payable receiver, string memory depositHash, uint256 gasFees ) public nonReentrant onlyExecutorOrOwner tokenChecks whenNotPaused {
+        require(receiver != address(0), "Bad receiver address");
          
         bytes32 hashSendTransaction = keccak256(
             abi.encode(
                 tokenAddress,
                 amount,
-                destination,
+                receiver,
                 keccak256(bytes(depositHash))
             )
         );
-        require(sendFundsMap[hashSendTransaction],"Already Processed");
+        require(!sendFundsMap[hashSendTransaction],"Already Processed");
+
+        uint256 bcnmyCommission = amount.mul(biconomyCommission).div(100);
+        uint256 amountToTransfer = amount.sub(bcnmyCommission.add(gasFees));
 
         if (tokenAddress == NATIVE) {
-            require(address(this).balance > amount, "Not Enough Balance");
-            require(destination.send(amount), "Native Transfer Failed");
+            require(address(this).balance > amountToTransfer, "Not Enough Balance");
+            require(receiver.send(amountToTransfer), "Native Transfer Failed");
         } else {
             IERC20 erc20 = IERC20(tokenAddress);
-            require(erc20.transfer(destination,amount),"Token Transfer Failed");
+            require(erc20.balanceOf(address(this)) > amountToTransfer, "Not Enough Balance");
+            require(erc20.transfer(receiver,amountToTransfer),"Token Transfer Failed");
         }
 
         sendFundsMap[hashSendTransaction] = true;
-        emit AssetSent(tokenAddress, amount, tokenAddress);
-    }
-
-    function sendBulk( ReceiverData[] memory data ) public nonReentrant onlyExecutorOrOwner whenNotPaused {
-        for (uint256 i = 0; i < data.length; i++) {
-            require(data[i]._tokenAddress != address(0), "Bad tokenAddress");
-            require(data[i]._amount != 0, "Bad Amount");
-            send(data[i]._tokenAddress, data[i]._amount, data[i]._destinations, data[i]._depositHash);
-        }
+        emit AssetSent(tokenAddress, amountToTransfer, receiver);
     }
 }
