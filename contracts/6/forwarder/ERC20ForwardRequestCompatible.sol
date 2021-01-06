c34c8b28647ddc60a34b2c141204a949d7a41108
livingrock7 <chiragtitiya@exvillage.com>
2021-01-06 18:46:05 +0530

    comments regarding forward request

--- contracts/6/forwarder/ERC20ForwardRequestCompatible.sol
+++ contracts/6/forwarder/ERC20ForwardRequestCompatible.sol
@@ -4,15 +4,19 @@ pragma experimental ABIEncoderV2;
 contract ERC20ForwardRequestTypes{
 
     struct ERC20ForwardRequest {
-        address from; //index
-        address to; //index
-        address token; //index
+        address from; 
+        address to; 
+        address token; 
         uint256 txGas;
         uint256 tokenGasPrice;
-        uint256 batchId; //maybe
-        uint256 batchNonce; //maybe
-        uint256 deadline;
+        uint256 batchId; 
+        uint256 batchNonce; 
+        uint256 deadline; 
         bytes data;
     }
     
 }
+
+// token gas price is : ( gas price * 10^ token decimals ) / (token prince in USD / ETH price in USD)
+// deadline can be removed : GSN reference https://github.com/opengsn/gsn/blob/master/contracts/forwarder/IForwarder.sol
+// 1D nonces could be used
