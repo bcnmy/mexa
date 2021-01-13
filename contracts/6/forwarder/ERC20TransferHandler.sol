4a57f5161768de66fcc14e25623219c3ac6a5f27
livingrock7 <chiragtitiya@exvillage.com>
2021-01-13 13:22:28 +0530

    remove comment

--- contracts/6/forwarder/ERC20TransferHandler.sol
+++ contracts/6/forwarder/ERC20TransferHandler.sol
@@ -6,7 +6,6 @@ pragma experimental ABIEncoderV2;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@opengsn/gsn/contracts/BaseRelayRecipient.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ERC20TransferHandler is BaseRelayRecipient{
 
