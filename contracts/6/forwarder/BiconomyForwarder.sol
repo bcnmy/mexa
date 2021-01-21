d9ac14a2cf849e800b8f05bb5af1feaabc7c1e19
livingrock7 <chiragtitiya@exvillage.com>
2021-01-21 11:48:19 +0530

    chainId to salt in domain seperator

--- contracts/6/forwarder/BiconomyForwarder.sol
+++ contracts/6/forwarder/BiconomyForwarder.sol
@@ -25,7 +25,7 @@ contract BiconomyForwarder is ERC20ForwardRequestTypes,Ownable{
 
     mapping(bytes32 => bool) public domains;
 
-    string public constant EIP712_DOMAIN_TYPE = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
+    string public constant EIP712_DOMAIN_TYPE = "EIP712Domain(string name,string version,uint256 salt,address verifyingContract)";
 
     bytes32 public constant REQUEST_TYPEHASH = keccak256(bytes("ERC20ForwardRequest(address from,address to,address token,uint256 txGas,uint256 tokenGasPrice,uint256 batchId,uint256 batchNonce,uint256 deadline,bytes data)"));
 
