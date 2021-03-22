3987af25dafa20e3c4f43421db00c3a63377dafb
livingrock7 <chiragtitiya@exvillage.com>
2021-03-23 02:01:14 +0530

    trusted forwarder deployer matic

--- scripts/gas-price/get-gas-price.js
+++ scripts/gas-price/get-gas-price.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const axios = require("axios");
 const etherscanApiKey = fs.readFileSync(".etherscan").toString().trim();
 const etherscanApi = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${etherscanApiKey}`;
+const maticGasStationApi = 'https://gasstation-mainnet.matic.network';
 
 function estimateGasPrice () {
   if (etherscanApi) {
@@ -33,4 +34,38 @@ function estimateGasPrice () {
       });
   });
 };
-module.exports = { estimateGasPrice };
+
+function estimateGasPriceMatic () {
+  if (etherscanApi) {
+    let url = maticGasStationApi.split("?");
+    url = url[0];
+    console.log(`Getting gas price from Matic gas station for Matic Mainnet using URL ${url}`);
+  }
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(maticGasStationApi)
+      .then(function (response) {
+        if (response && response.data) {
+          console.log(response.data);
+          const mediumGasPrice = response.data.standard;
+          const mediumGasPriceInWei = mediumGasPrice * 1e9; // gasPrice is in 1 Gwei unit
+          console.log(`Medium GasPrice for Mainnet from Matic gas station api is ${mediumGasPriceInWei} wei`);
+
+          const fastGasPrice = response.data.fast;
+          const fastGasPriceInWei = fastGasPrice * 1e9; // gasPrice is in 1 Gwei unit
+          console.log(`Fast GasPrice for Mainnet from Matic gas station api is ${fastGasPriceInWei} wei`);
+          resolve({ mediumGasPriceInWei, fastGasPriceInWei });
+        } else {
+          console.log(`Etherscan api reponse status ${response.status} and response data ${response.data}`);
+          reject(new Error("There is some problem in fetching gasPrice from etherscan"));
+        }
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+};
+
+
+module.exports = { estimateGasPrice, estimateGasPriceMatic };
